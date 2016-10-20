59ff326e7fe9a0c8acbf8d80e8a131b29e8406e8
somshubham <somshubhams@gmail.com>
2016-10-20 23:41:44 +0530

    asdad

--- app/src/main/java/com/mbma/som/som/MainActivity.java
+++ app/src/main/java/com/mbma/som/som/MainActivity.java
@@ -8,6 +8,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);setContentView(R.layout.activity_main);setContentView(R.layout.activity_main);
+//kjhkjhkjhkjhkjhkjhkjhkjhkjh
+        //kkkkkkkkkk
+
+
+       int skjdasjd;
+       int asjkdkasd;
+
+
     }
 }
